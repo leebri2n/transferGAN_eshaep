50b89fcaf6ecfb0c05bfe54033ef1d92b9db396b
leebri2n <74383974+leebri2n@users.noreply.github.com>
2022-06-01 12:33:39 -0400

    can't get by hashtag

--- gans_w1/gans02_collection.py
+++ gans_w1/gans02_collection.py
@@ -5,12 +5,14 @@ args = {"login_user": "gramy.scrape",
         'media_types': ['image', 'story-image'],
         'tag': True}
 
-insta_scraper = instagram_scraper.InstagramScraper(**args)
+insta_scraper = instagram_scraper.InstagramScraper(login_user = 'gramy.scrape',
+                                                    login_pass = 'insta$8scrape',
+                                                    media_types = ['image', 'story-image'],
+                                                    tag='attackontitaneditsb')
+
 insta_scraper.authenticate_with_login()
 
-print(insta_scraper.tag)
-#insta_scraper.usernames.append('dprlive')
-insta_scraper.usernames.append('attackontitaneditsb')
+insta_scraper.usernames.append('dprlive')
 
-insta_scraper.query_hashtag_gen('attackontitaneditsb')
+
 insta_scraper.scrape()

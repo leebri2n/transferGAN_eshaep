68676e2725dda91b530ef126dde8ef66ea6debaa
leebri2n <74383974+leebri2n@users.noreply.github.com>
2022-06-02 10:35:09 -0400

    collection more robust

--- gans_w1/gans02_collection.py
+++ gans_w1/gans02_collection.py
@@ -1,38 +1,39 @@
-"""
-import instagram_scraper
-
-args = {"login_user": "gramy.scrape",
-        "login_pass": "insta$8scrape",
-        "media_types": ["image", "story-image"],
-        "tag": True}
-
-insta_scraper = instagram_scraper.InstagramScraper(login_user = "gramy.scrape",
-                                                    login_pass = "insta$8scrape",
-                                                    media_types = ["image", "story-image"])
-
-insta_scraper.authenticate_with_login()
-shared_data = insta_scraper.get_shared_data_userinfo(username='dprlive')
-shared_data = insta_scraper.get_shared_data_userinfo(username='chingooz')
-
-
-insta_scraper.usernames.append("dprlive")
-insta_scraper.usernames.append("chingooz")
-
-print(insta_scraper.usernames)
-
-insta_scraper.scrape()al
-"""
-
 import instaloader
 from datetime import datetime
 from itertools import dropwhile, takewhile
 import csv
+import os
 
-class GetInstagramProfile():
-    def __init__(self) -> None:
-        self.L = instaloader.Instaloader()
-        self.L.login('gramy.scrape', 'insta$8scrape')
+destination = r'C:\Users\leebr\Documents\GitHub\data\gans'
 
+class InstagramScraper():
+    """
+        Class credit to:
+    """
+    def __init__(self, login_user='', login_pass='', dest_path='', \
+        date_start=(2022, 5, 1), date_end=(2022, 6, 1)) -> None:
+
+        self.L = instaloader.Instaloader(dirname_pattern=destination,
+            download_videos=False,
+            download_video_thumbnails=True,
+            save_metadata=False,
+            compress_json=False)
+
+        self.L.login(login_user, login_pass)
+        self.date_start = datetime(date_start[0], date_start[1], date_start[2])
+        self.date_end = datetime(date_end[0], date_end[1], date_end[2])
+
+    #HASHTAG
+    def download_hastag_posts(self, hashtag):
+        self.L.dirname_pattern=os.path.join(destination, hashtag) #@leebri2n
+        for post in instaloader.Hashtag.from_name(self.L.context, hashtag).get_posts():
+            try:
+                self.L.download_post(post, target='#'+hashtag)
+            except: #@leebri2n
+                print("ERROR ENCOUNTERED!")
+                continue
+
+    #USERS
     def download_users_profile_picture(self,username):
         self.L.download_profile(username, profile_pic_only=True)
 
@@ -44,17 +45,10 @@ class GetInstagramProfile():
         for post in takewhile(lambda p: p.date > SINCE, dropwhile(lambda p: p.date > UNTIL, posts)):
             self.L.download_post(post, username)
 
-    def download_hastag_posts(self, hashtag):
-        for post in instaloader.Hashtag.from_name(self.L.context, hashtag).get_posts():
-            try:
-                self.L.download_post(post, target='#'+hashtag)
-            except: #@leebri2ns
-                print("ERROR ENCOUNTERED!")
-                continue
-
     def get_users_followers(self,user_name):
         '''Note: login required to get a profile's followers.'''
-        self.L.login(input("input your username: gramy.scrape"), input("input your username: insta$8scrape") )
+        self.L.login(input("input your username: gramy.scrape"), \
+            input("input your username: insta$8scrape") )
         profile = instaloader.Profile.from_username(self.L.context, user_name)
         file = open("follower_names.txt","a+")
         for followee in profile.get_followers():
@@ -72,6 +66,7 @@ class GetInstagramProfile():
             file.write(username + "\n")
             print(username)
 
+    #POSTS
     def get_post_comments(self,username):
         posts = instaloader.Profile.from_username(self.L.context, username).get_posts()
         for post in posts:
@@ -87,7 +82,7 @@ class GetInstagramProfile():
             writer = csv.writer(file)
             posts = instaloader.Profile.from_username(self.L.context, username).get_posts()
             for post in posts:
-                print("post date: "+str(post.date))
+                print("post date: " + str(post.date))
                 print("post profile: "+post.profile)
                 print("post caption: "+post.caption)
                 print("post location: "+str(post.location))
@@ -103,5 +98,5 @@ class GetInstagramProfile():
                     print("comment date : "+str(comment.created_at_utc))
                 print("\n\n")
 
-cls = GetInstagramProfile()
-cls.download_hastag_posts("uvaldephotography")
+cls = InstagramScraper(login_user='gramy.scrape', login_pass='insta$8scrape', dest_path=destination)
+cls.download_hastag_posts("tacos")

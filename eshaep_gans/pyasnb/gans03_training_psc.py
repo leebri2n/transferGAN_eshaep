020a7189883ac8acfa2413d7b2f114c0c9878509
leebri2n <74383974+leebri2n@users.noreply.github.com>
2022-06-17 13:25:12 -0400

    minor changes

--- eshaep_gans/pyasnb/gans03_training_psc.py
+++ eshaep_gans/pyasnb/gans03_training_psc.py
@@ -18,32 +18,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from keras.utils.np_utils import to_categorical
 
-def gen_cnn():
-    model = Sequential()
-
-    model.add(Conv2D(1024, (2, 2), padding='same', input_shape=(1,4)))
-    model.add(Activation('relu'))
-
-    model.add(Conv2D(512, (2,2), padding='same'))
-    model.add(Activation('relu'))
-
-    model.add(Conv2D(256, (2,2), padding='same'))
-    model.add(Activation('relu'))
-
-    model.add(Conv2D(128, (2,2), padding='same'))
-    model.add(Activation('relu'))
-
-    model.add(Conv2D(3, (2,2), padding='same'))
-    model.add(Activation('relu'))
-
-    opt = keras.optimizers.RMSprop(learning_rate=0.0001, decay=1e-6)
-    model.compile(loss='categorical_crossentropy',
-                optimizer=opt,
-                metrics=['accuracy'])
-    print(model.summary())
-
-    return model
-
 
 """
     Attempt 2
@@ -65,3 +39,27 @@ dim = 1024 #assuming square images.
 channels = 1 #3?
 
 img_shape = (dim, dim, channels)
+
+def gen_cnn(z_shape):
+    model = Sequential()
+
+    model.add(Dense(12544, input_dim = z_shape))
+    model.add(Reshape((7, 7, 256)))
+
+    model.add(Conv2DTranspose(128, kernel_size=3, strides=2, padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.01))
+
+    model.add(Conv2DTranpose(64, kernel_size=3, strides=2, padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.01))
+
+    model.add(Conv2DTranspose(1, kernel_size=3, strides=2, padding='same'))
+    model.add(Activation('tanh'))
+
+    #opt = keras.optimizers.RMSprop(learning_rate=0.0001, decay=1e-6)
+    model.compile(loss='categorical_crossentropy',
+                optimizer=opt,
+                metrics=['accuracy'])
+
+    return model

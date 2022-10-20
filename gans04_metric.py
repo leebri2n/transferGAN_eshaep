f7e6f8cfdb28352a2aec6322ede9024620a61d14
leebri2n <74383974+leebri2n@users.noreply.github.com>
2022-10-20 02:54:29 -0400

    Metric graphic coder

--- gans04_metric.py
+++ gans04_metric.py
@@ -1,28 +1,35 @@
-import cv2
 import os
-from os import listdir
 import sys
-import argparse
-from posixpath import join
+import cv2
 import shutil
+import argparse
+from os import listdir
+from posixpath import join
 
 import json
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import face_recognition as fr
-import imquality.brisque as brisque
-
-import imutils
 import time
-from imutils.object_detection import non_max_suppression
-#from google.colab.patches import cv2_imshow
 from tqdm import tqdm
 
 from datetime import datetime
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-import PIL
-from PIL import Image, ImageStat
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class MetricDisplay():
+    def __init__(self):
+        self.start = 0
+
+    def read_json(self, path):
+        with open(path, 'r') as json_file:
+            fid50k = json.load(json_file)
+            print(type(fid50k))
+
+        return fid50k
+
+m = MetricDisplay()
+print(m.read_json('./metric-fid50k_full.json'))

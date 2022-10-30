95b1214b5423f6a5bfc1e1071aecf193606b6048
leebri2n <74383974+leebri2n@users.noreply.github.com>
2022-10-30 18:56:03 -0400

    More visualization tech

--- gans04_metric.py
+++ gans04_metric.py
@@ -23,16 +23,19 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 class MetricDisplay():
-    def __init__(self, in_dir, out_dir):
+    def __init__(self, in_dir, out_dir, type):
         self.metriclog = []
         self.in_dir = in_dir
         self.out_dir = out_dir
+        self.type = type
+        self.tnrfont = {'fontname':'Times New Roman'}
 
-        plt.figure(figsize=(10,8))
+
+        plt.figure(figsize=(9,6))
         plt.yscale('log')
-        plt.title('Transfer Learning Performance', fontname="Times")
-        plt.xlabel("Training Length (kimg)")
-        plt.ylabel("Performance (FID)")
+        plt.grid()
+        plt.xlabel("Training Length (kimg)", fontsize=12, verticalalignment='top', **self.tnrfont)
+        plt.ylabel("Performance (FID)", fontsize=12, verticalalignment='bottom', **self.tnrfont)
 
 
     def read_json(self, in_file):
@@ -48,14 +51,26 @@ class MetricDisplay():
 
     def visualize_fid_line(self, in_file, col, lab):
         score_data = self.read_json(os.path.join(self.in_dir, in_file))
-        print(score_data)
+        #print(score_data)
 
         plt.plot(np.linspace(1, len(score_data), len(score_data), endpoint=True), score_data, \
             color=col, label=lab)
-        self.visualize()
 
-    def visualize(self):
-        plt.legend()
+        self.visualize(self.type)
+
+    def visualize(self, type):
+        #print(type)
+        legend = plt.legend()
+        if type == 'dataset':
+            plt.title('Training Performance Across Target Datasets', \
+                fontsize=20, verticalalignment='bottom', **self.tnrfont)
+            legend.set_title('Target Datasets')
+        elif type == 'network':
+            plt.title('Training Performance Across Starting Network', \
+                fontsize=20, verticalalignment='bottom', **self.tnrfont)
+            legend.set_title('Pretrained Network')
+        else:
+            raise Exception("Invalid experiment type.")
 
 
 in_dir = os.path.join(os.getcwd(), 'fidscoring')
@@ -65,11 +80,17 @@ print(matplotlib.get_cachedir())
 print(in_dir)
 print(out_dir)
 
-vis = MetricDisplay(in_dir, out_dir)
-vis.visualize_fid_line('062-metric-fid50k_full-stock.json', 'orange', 'Beach sunset')
-vis.visualize_fid_line('087-metric-fid50k_full-jet.json', 'coral', 'Fighter jet')
-vis.visualize_fid_line('088-metric-fid50k_full-latte.json', 'sienna', 'Latte art')
-vis.visualize_fid_line('089-metric-fid50k_full-betta.json', 'aqua', 'Betta fish')
-vis.visualize_fid_line('099-metric-fid50k_full-trains.json', 'tomato', 'Train')
-vis.visualize_fid_line('100-metric-fid50k_full-corgi.json', 'green', 'Corgi')
+vis_dataset = MetricDisplay(in_dir, out_dir, 'dataset')
+vis_dataset.visualize_fid_line('087-metric-fid50k_full-jet.json', 'green', 'Fighter jet')
+vis_dataset.visualize_fid_line('099-metric-fid50k_full-trains.json', 'lightgreen', 'Train')
+vis_dataset.visualize_fid_line('088-metric-fid50k_full-latte.json', 'tomato', 'Latte art')
+vis_dataset.visualize_fid_line('089-metric-fid50k_full-betta.json', 'blue', 'Betta fish')
+vis_dataset.visualize_fid_line('100-metric-fid50k_full-corgi.json', 'darkblue', 'Corgi')
+vis_dataset.visualize_fid_line('062-metric-fid50k_full-stock.json', 'violet', 'Beach sunset')
+
+vis_network = MetricDisplay(in_dir, out_dir, 'network')
+vis_network.visualize_fid_line('088-metric-fid50k_full-latte.json', 'tomato', 'Animal Faces')
+vis_network.visualize_fid_line('093-metric-fid50k_full-latteff.json', 'blue', 'Flickr Faces')
+vis_network.visualize_fid_line('095-metric-fid50k_full-lattemet.json', 'green', 'Metfaces')
+
 plt.show()

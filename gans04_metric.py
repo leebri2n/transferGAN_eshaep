5ae1b79fa2aefa0c3a9d8fb50ddd04e67334f3bf
leebri2n <74383974+leebri2n@users.noreply.github.com>
2022-11-04 02:09:46 -0400

    Working metric vis

--- gans04_metric.py
+++ gans04_metric.py
@@ -36,6 +36,16 @@ class MetricDisplay():
         plt.xlabel("Training Length (kimg)", fontsize=12, verticalalignment='top', **self.tnrfont)
         plt.ylabel("Performance (FID)", fontsize=12, verticalalignment='bottom', **self.tnrfont)
 
+    def reformat(self, in_file):
+        in_file = os.path.join(self.in_dir, in_file)
+        with open(in_file, 'r') as fid_file:
+            lines = fid_file.readlines()
+        with open(in_file, 'w') as fid_file:
+            for iter, line in enumerate(lines):
+                if iter == 0: continue
+                snap = line.strip().split(',')
+                if int(snap[1]) % 80 == 0 or int(snap[1]) == 3500:
+                    fid_file.write(line)
 
     def read_file(self, in_file):
         score_data = []
@@ -46,7 +56,7 @@ class MetricDisplay():
                     if iter == 0: continue
                     snap = line.strip().split(',')
                     #print(snap[2])
-                    score_data.append(snap[2])
+                    score_data.append(float(snap[2]))
         else:
             with open(in_file, 'r') as fid_file:
                 for snap in fid_file:
@@ -60,6 +70,7 @@ class MetricDisplay():
     def visualize_fid_line(self, in_file, col, lab):
         score_data = self.read_file(os.path.join(self.in_dir, in_file))
         #print('SCORES', score_data, print(type(score_data)))
+        #print(len(score_data))
 
         plt.plot(np.linspace(1, len(score_data), len(score_data), endpoint=True), score_data, \
             color=col, label=lab)
@@ -101,6 +112,8 @@ vis_dataset.visualize_fid_line('099-metric-fid50k_full-trains.json', 'lightgreen
 vis_dataset.visualize_fid_line('088-metric-fid50k_full-latte.json', 'tomato', 'Latte art')
 vis_dataset.visualize_fid_line('089-metric-fid50k_full-betta.json', 'blue', 'Betta fish')
 vis_dataset.visualize_fid_line('100-metric-fid50k_full-corgi.json', 'darkblue', 'Corgi')
+vis_dataset.visualize_fid_line('098-metric-fid50k_full-baldeagle.json', 'pink', 'Bald Eagle')
+vis_dataset.visualize_fid_line('101-metric-fid50k_full-mountain.json', 'yellow', 'Mountain')
 vis_dataset.visualize_fid_line('062-metric-fid50k_full-stock.json', 'violet', 'Beach sunset')
 
 vis_network = MetricDisplay(in_dir, out_dir, 'network')
@@ -108,8 +121,16 @@ vis_network.visualize_fid_line('088-metric-fid50k_full-latte.json', 'tomato', 'A
 vis_network.visualize_fid_line('093-metric-fid50k_full-latteff.json', 'blue', 'Flickr Faces')
 vis_network.visualize_fid_line('095-metric-fid50k_full-lattemet.json', 'green', 'Metfaces')
 
+vis_network2 = MetricDisplay(in_dir, out_dir, 'network')
+vis_network2.visualize_fid_line('087-metric-fid50k_full-jet.json', 'tomato', 'Animal Faces')
+vis_network2.visualize_fid_line('092-metric-fid50k_full-jetff.json', 'blue', 'Flickr Faces')
+vis_network2.visualize_fid_line('094-metric-fid50k_full-jetmet.json', 'green', 'Metfaces')
+
 vis_aug = MetricDisplay(in_dir, out_dir, 'aug')
-vis_aug.visualize_fid_line('run-train02_070-gamma15-tag-Metrics_fid50k_full.csv', 'lime', 'ADA')
+vis_aug.reformat('run-train01_070-gamma15-tag-Metrics_fid50k_full.csv')
+vis_aug.reformat('run-train02_078-augfixed-tag-Metrics_fid50k_full.csv')
+
+vis_aug.visualize_fid_line('run-train01_070-gamma15-tag-Metrics_fid50k_full.csv', 'blue', 'ADA')
 vis_aug.visualize_fid_line('run-train02_077-augnone-tag-Metrics_fid50k_full.csv', 'tomato', 'No augmentation')
 vis_aug.visualize_fid_line('run-train02_078-augfixed-tag-Metrics_fid50k_full.csv', 'lime', 'Fixed augmentation')
 plt.show()
